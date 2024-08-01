511c9e75574d91a5756989c6a9f5d80ff4d5d251
abdelaleemadel <ahmedadel2912@gmail.com>
2024-08-01 15:11:04 +0300

    Build App Layout

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,24 @@ import Settings from "./pages/Settings";
 import Users from "./pages/Users";
 import Account from "./pages/Account";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 function App() {
   return (
     <>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate to="dashboard" replace />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate to="dashboard" replace />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<Users />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
           <Route path="login" element={<Login />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="users" element={<Users />} />
-          <Route path="account" element={<Account />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
