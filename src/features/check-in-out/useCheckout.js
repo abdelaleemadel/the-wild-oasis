2178136082af5295480d884af59642c6b5bf8379
abdelaleemadel <ahmedadel2912@gmail.com>
2024-08-28 02:47:37 +0300

    Today's Activity

--- src/features/check-in-out/useCheckout.js
+++ src/features/check-in-out/useCheckout.js
@@ -3,19 +3,18 @@ import { updateBooking } from "../../services/apiBookings";
 import toast from "react-hot-toast";
 
 export function useCheckout() {
+  const queryClient = useQueryClient();
+  const { mutate: checkout, isLoading: isCheckingOut } = useMutation({
+    mutationFn: (bookingId) =>
+      updateBooking(bookingId, {
+        status: "checked-out",
+      }),
+    onSuccess: (data) => {
+      toast.success(`Booking #${data.id} successfully checked out`);
+      queryClient.invalidateQueries({ active: true });
+    },
+    onError: () => toast.error("There was an error while checking out"),
+  });
 
-    const queryClient = useQueryClient();
-    const { mutate: checkout, isLoading: isCheckingOut } = useMutation({
-        mutationFn: (bookingId) => updateBooking(bookingId, {
-            status: "checked-out",
-        }),
-        onSuccess: (data) => {
-            toast.success(`Booking #${data.id} successfully checked out`);
-            queryClient.invalidateQueries({ active: true });
-
-        },
-        onError: () => toast.error("There was an error while checking out")
-    });
-
-    return { checkout, isCheckingOut }
+  return { checkout, isCheckingOut };
 }
--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Stats from "./Stats";
 import useCabins from "./../cabins/useCabins";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -33,7 +34,7 @@ function DashboardLayout() {
         bookings={bookings}
         numOfDays={numOfDays}
       />
-      <div>Today&apos;s activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numOfDays={numOfDays} />
     </StyledDashboardLayout>
