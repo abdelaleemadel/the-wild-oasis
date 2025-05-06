a1d13fb8a9bb5ede3839d4107bb4d5955c13e5e5
abdelaleemadel <ahmedadel2912@gmail.com>
2025-05-06 11:40:04 +0300

    Fix the main layout in small screens

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -19,6 +19,9 @@ const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
   overflow: auto;
+  position: fixed;
+  width: 100%;
+  height: 100%;
 `;
 const Container = styled.div`
   max-width: 120rem;
